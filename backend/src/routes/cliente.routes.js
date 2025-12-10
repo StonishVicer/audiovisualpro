82496373d2f993511607fc52478ab05781b4e0b2
colmen4z <saircolmenarez2006@gmail.com>
2025-12-10 11:18:13 -0400

    Se estan realizando las busquedas en vivo - Sidebar modificada - se pueden editar clientes

--- backend/src/routes/cliente.routes.js
+++ backend/src/routes/cliente.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { createCliente, getClientes, getClienteById, deleteCliente } from '../controllers/cliente.controller.js'
+import { createCliente, getClientes, getClienteById, deleteCliente, updateCliente } from '../controllers/cliente.controller.js'
 
 const router = Router()
 router.post('/', createCliente)
 router.get('/', getClientes)
 router.get('/:id', getClienteById)
 router.delete('/:id', deleteCliente)
+router.put('/:id', updateCliente)
 
 export default router
